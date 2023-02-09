ed219d99676b4339b5c9fd35a0a672bf7c9f4e01
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-02-08 23:01:22 -0600

    UPDATE

--- blog-analisis-datos/grafico-de-caja-con-r-python/render-markdown.R
+++ blog-analisis-datos/grafico-de-caja-con-r-python/render-markdown.R
@@ -1,3 +1,3 @@
 ruta <- paste0(getwd(),
-               '/blog-analisis-datos/conexion-con-bases-de-datos-con-r/conexion-con-bases-de-datos-con-r.Rmd')
+               '/blog-analisis-datos/grafico-de-caja-con-r-python/grafico-de-caja-con-r-python.Rmd')
 rmarkdown::render(ruta)
