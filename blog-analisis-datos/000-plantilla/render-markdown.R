f0db5d63100a62c2f228187f07594e12262d14c1
LASPUMSS <umssluisalbertosolisp@gmail.com>
2023-02-08 22:55:02 -0600

    UPDATE

---
+++ blog-analisis-datos/000-plantilla/render-markdown.R
@@ -0,0 +1,3 @@
+ruta <- paste0(getwd(),
+               '/blog-analisis-datos/conexion-con-bases-de-datos-con-r/conexion-con-bases-de-datos-con-r.Rmd')
+rmarkdown::render(ruta)
---
+++ blog-analisis-datos/grafico-de-barras-con-r-python/render-markdown.R
@@ -0,0 +1,3 @@
+ruta <- paste0(getwd(),
+               '/blog-analisis-datos/grafico-de-barras-con-r-python/grafico-de-barras-con-r-python.Rmd')
+rmarkdown::render(ruta)
